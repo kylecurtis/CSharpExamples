08a230e03fc92281c0e25c62a58ac3c13b51c8ca
Kyle Curtis <30717363+kylecurtis@users.noreply.github.com>
2024-05-06 05:46:16 -0400

    Add project files.

---
+++ CSharpExamples/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace CSharpExamples
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
