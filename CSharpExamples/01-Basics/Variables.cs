2d3afc45ebe5dc9eb9c0500e89a705f00513eaa1
Kyle Curtis <30717363+kylecurtis@users.noreply.github.com>
2024-05-06 21:27:47 -0400

    Add variables class

---
+++ CSharpExamples/01-Basics/Variables.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace CSharpExamples._01_Basics
+{
+    internal class Variables
+    {
+        public static void Run()
+        {
+            // Title
+            Console.WriteLine("Variables:");
+
+            // Variable Declaration
+            int noNumber;
+            
+            // Variable Initialization
+            int firstNumber = 0;
+
+            // Constant Variables:
+            // Constant values must be initialzied when declared using the `const` keyword.
+            // Constants are immutable values, and can not be changed by the program.
+            const int DaysInWeek = 7;
+
+            // Variables are case-sensitive.
+            // For example, these two variables are completely different.
+            int number = 0;
+            int Number = 1;
+
+            // Variable Naming Conventions:
+            // Camel Case is used for local variable names (firstName, playerScore, etc.).
+            // Pascal Case is used for constant variable names (MaxLevel, YearOfBirth, etc.).
+            int playerScore = 32;
+            const int MaxLevel = 99;
+            
+            // Variable Redeclaration:
+            // In C#, you can assign a new value to an existing variable, but you cannot
+            // re-declare the same variable within the same scope.
+            int x = 0;
+            Console.WriteLine($"x is currently: {x}");
+            
+            x = 10;
+            Console.WriteLine($"x is now: {x}");
+
+            // Return Values
+            Console.WriteLine($"First Number: {firstNumber}");
+            Console.WriteLine($"Days in a week: {DaysInWeek}");
+            Console.WriteLine($"number is: {number}");
+            Console.WriteLine($"Number is: {Number}");
+            Console.WriteLine($"Player Score: {playerScore}");
+            Console.WriteLine($"Max Level: {MaxLevel}");
+        }
+    }
+}
--- CSharpExamples/Program.cs
+++ CSharpExamples/Program.cs
@@ -1,10 +1,47 @@
-﻿namespace CSharpExamples
+﻿using CSharpExamples._01_Basics;
+
+namespace CSharpExamples
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            static void Line()
+            {
+                Console.WriteLine("===========================================================");
+            } 
+
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Select an option:");
+                Console.WriteLine("1. Variables");
+                Console.WriteLine("0. Exit");
+                Console.WriteLine();
+                Console.Write("Enter your choice: ");
+
+                var choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        Console.Clear();
+                        Console.WriteLine();
+                        Variables.Run();
+                        Console.WriteLine();
+                        Line();
+                        break;
+                    case "0":
+                        return;
+                    default:
+                        Console.Clear();
+                        Console.WriteLine();
+                        Console.WriteLine("Invalid choice, please try again.");
+                        Console.WriteLine();
+                        Line();
+                        break;
+                }
+            }
         }
     }
 }
